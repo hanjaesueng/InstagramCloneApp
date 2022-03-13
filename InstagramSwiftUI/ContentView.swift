4a62b2536812c628aafe8d4d60ae8c7988f5b1fd
jshan <jshan@hanamicron.co.kr>
2022-03-13 13:11:15 +0900

    Initial Commit

---
+++ InstagramSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramSwiftUI
+//
+//  Created by jaeseung han on 2022/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by jaeseung han on 2022/03/13.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
